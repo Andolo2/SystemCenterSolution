189396e2decbe53ee8837cc176b8b8091032b028
Andolo2 <andreasolofsson123@gmail.com>
2023-02-27 14:53:50 +0100

    Add project files.

---
+++ SystemCenter01/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCenter01.Context
+{
+    internal class DataContext : DbContext
+    {
+
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions options) : base(options) // Add <Tickets>
+        {
+        }
+    }
+}
